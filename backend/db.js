b021b01dad0e3872622d43500bb86e14f7867908
Aaditya Kapruwan <kapruwanaaditya@gmail.com>
2024-09-19 22:20:14 +0530

    CollabPad

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-// db.js
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/notepad', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch((error) => console.error('Error connecting to MongoDB:', error.message));
